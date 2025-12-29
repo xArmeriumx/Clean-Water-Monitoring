8011ab805a36a49dfb8c2285d1c363b27d3a6f2a
xArmeriumx <armnapat.a.03@gmail.com>
2025-12-29 22:39:21 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../server.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../cert.pem')),
-    },
+    https: 
+      fs.existsSync(path.resolve(__dirname, '../server.key')) && 
+      fs.existsSync(path.resolve(__dirname, '../cert.pem'))
+        ? {
+            key: fs.readFileSync(path.resolve(__dirname, '../server.key')),
+            cert: fs.readFileSync(path.resolve(__dirname, '../cert.pem')),
+          }
+        : undefined,
     proxy: {
       '/api': {
         target: 'https://localhost:5000',
